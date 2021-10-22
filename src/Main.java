5dc1158530f84f028b9d8e1713ee7ce5b6d421a0
Ольга Крылова <>
2021-10-22 15:11:53 +0300

    Create methods for arrays and tests

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import game.redgreen.Game;
+import game.redgreen.SpeedArrays;
+
+import java.util.Arrays;
 
 import static game.redgreen.Game.isLoser;
 
@@ -22,5 +24,17 @@ public class Main {
         }
 
         System.out.println("Всего выбыло " + loserCount + " игроков");
+
+        int[] speeds = {15, 5, 0, 3, 0};
+
+        loserCount = SpeedArrays.countLoser(speeds);
+        int[] speedLosers = SpeedArrays.speedLosers(speeds);
+        int[] speedWinners = SpeedArrays.speedWinners(speeds);
+
+        System.out.println("Среди " + speeds.length + " игроков: \n" +
+                "Выбыло " + loserCount + " игроков, осталось " + (speeds.length - loserCount) + " игроков \n" +
+                "Скорости выбывших: " + Arrays.toString(speedLosers) +"\n" +
+                "Скорости оставшихся: " + Arrays.toString(speedWinners)
+        );
     }
 }
